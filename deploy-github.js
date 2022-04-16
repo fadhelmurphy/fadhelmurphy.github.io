449180afb40fafc0c44e46b32a417d9f678a08d7
Fadhel ijlal falah <fadhelijlalfalah@gmail.com>
2022-04-16 20:41:50 +0700

    automation

--- deploy-github.js
+++ deploy-github.js
@@ -5,7 +5,7 @@ ghpages.publish(
   "public",
   {
     branch: "master",
-    repo: "git@github.com:fadhelmurphy/fadhelmurphy.github.io.git",
+    repo: "https://github.com/fadhelmurphy/fadhelmurphy.github.io.git",
   },
   () => {
     console.log("Deploy Complete!")
