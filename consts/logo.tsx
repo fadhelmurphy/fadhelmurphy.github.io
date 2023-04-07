f21d5271fd497c51d5f2737cc533bd36a4351537
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2023-04-07 13:40:42 +0700

    update astro

---
+++ consts/logo.tsx
@@ -0,0 +1,75 @@
+import { prefix } from "./config";
+
+export type SliderDataType = {
+    [key:string]: string | boolean;
+}
+
+export const SliderData: SliderDataType[] = [
+  {
+    type: "text",
+    text: "React",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/webpack.png`,
+    alt: "webpack fadhel",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/nextjs-icon.png`,
+    alt: "nextjs fadhel",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/gatsby-icon.png`,
+    alt: "gatsby fadhel",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/flutter.png`,
+    alt: "flutter fadhel",
+  },
+  {
+    type: "text",
+    text: "MobX",
+  },
+  {
+    type: "text",
+    text: "Zustand",
+  },
+  {
+    type: "text",
+    text: "Redux",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/tensorflow-logo.png`,
+    alt: "tensorflow fadhel",
+  },
+  {
+    type: "text",
+    text: "Sklearn",
+  },
+  {
+    type: "text",
+    text: "Express.js",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/mysql.png`,
+    alt: "mysql fadhel",
+  },
+  {
+    type: "image",
+    url: `${prefix}/home/stack-slider/laravel.png`,
+    alt: "laravel fadhel",
+  },
+  {
+    type: "text",
+    text: "GraphQL",
+  },
+  {
+    type: "text",
+    text: "MongoDB",
+  },
+];
