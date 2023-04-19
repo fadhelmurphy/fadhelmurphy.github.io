b1ce92b986d80a2ace2a8961b0263bafdfc9d808
Fadhel Pop <fdn-fadelijlalfalah@FDN-FADELs-MacBook-Pro.local>
2023-04-19 22:51:22 +0700

    feat: add portfolio section

--- containers/sections/portfolio.tsx
+++ containers/sections/portfolio.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { TimelineSectionsType } from "Containers/types/sections.type";
 
 const Portfolio = ({ title, desc, children }: TimelineSectionsType) => {
   return (
     <>
     
-    <div className="portfolio-timeline-sections bg-edu-orange h-screen w-100 porto-clip py-72">
-      <div className="container mx-auto h-full">
+    <div className="portfolio-timeline-sections flex justify-center items-center relative h-[150vh] w-100 porto-clip">
+    <div className="w-full h-full bg-white-vertical-gradient absolute z-[2]" />
+    <div className="w-full h-full bg-edu-orange absolute z-[1]" />
+      <div className="container mx-auto h-full absolute z-[3] py-[30rem]">
         <div className="head h-4/6">
           <div className="title font-header text-center text-2xl font-semibold md:text-8xl text-white">
             <h2>{title}</h2>
