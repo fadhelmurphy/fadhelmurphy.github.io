fa468b67e38142586a4523bb76f9bbd6f90d639e
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2023-09-07 15:23:38 +0700

    chore: change prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import image from "@astrojs/image";
-import { prefix } from "./consts/config";
+// import { prefix } from "./consts/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), react(), image()],
   // site: `http://localhost:3000${prefix}`,
-  base: prefix,
-  assetsPrefix: prefix,
+  // base: prefix, # base prefix
+  // assetsPrefix: prefix, # asset prefix
   // publicDir: prefix,
   // build: {
   //   assetsPrefix: prefix
