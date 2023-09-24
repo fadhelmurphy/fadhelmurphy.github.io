96ea0ac074d42fc10b2e09126e53829532daf6e9
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2023-09-24 12:18:47 +0700

    fix: AOS.js

--- containers/sections/education.tsx
+++ containers/sections/education.tsx
@@ -22,7 +22,7 @@ const Education = ({ title, children }: TimelineSectionsType): JSX.Element => {
           <div className="head flex justify-center items-center relative">
             <div className="radial-white-gradient-bg" />
             <div className="pattern" />
-            <div className="title font-header text-center text-6xl md:text-7xl 2xl:text-8xl font-semibold bg-clip-text text-transparent bg-edu-orange z-[3] text-shadow-white">
+            <div data-aos="animation-scale-y" data-aos-duration="500" className="title font-header text-center text-6xl md:text-7xl 2xl:text-8xl font-semibold bg-clip-text text-transparent bg-edu-orange z-[3] text-shadow-white">
               <h2>{title}</h2>
             </div>
           </div>
