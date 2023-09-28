c281b652511bfcd2136eb2992492d28eb3dd3016
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2023-09-28 11:14:16 +0700

    chore: remove useEffect sucks

--- containers/sections/education.tsx
+++ containers/sections/education.tsx
@@ -1,36 +1,18 @@
 import { type TimelineSectionsType } from 'Containers/types/sections.type'
 import styles from './section.module.scss'
-import { useState, useEffect } from 'react'
 
-const Education = ({ title, children }: TimelineSectionsType): JSX.Element => {
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i.test(
-        navigator.userAgent
-      )
-    ) {
-      setIsMobile(true)
-    }
-  }, [])
-
-  return (
-    <>
-      <div className={`education-timeline-sections bg-section-edu md:bg-section-edu-md 2xl:bg-section-edu-lg pb-48 ${!isMobile ? styles.Section_education : styles.Section_education_mobile}`}>
-        <div className="mx-auto grid grid-cols-1 md:grid-cols-2">
-          <div className="head flex justify-center items-center relative">
-            <div className="radial-white-gradient-bg" />
-            <div className="pattern" />
-            <div data-aos="animation-scale-y" data-aos-duration="500" className="title font-header text-center text-6xl md:text-7xl 2xl:text-8xl font-semibold bg-clip-text text-transparent bg-edu-orange z-[3] text-shadow-white">
-              <h2>{title}</h2>
-            </div>
-          </div>
-          <div className="content my-12 p-8 md:p-0">{children}</div>
-        </div>
+const Education = ({ title, children }: TimelineSectionsType): JSX.Element =>
+<div className={`education-timeline-sections bg-section-edu md:bg-section-edu-md 2xl:bg-section-edu-lg pb-48 ${styles.Section_education}`}>
+  <div className="mx-auto grid grid-cols-1 md:grid-cols-2">
+    <div className="head flex justify-center items-center relative">
+      <div className="radial-white-gradient-bg" />
+      <div className="pattern" />
+      <div data-aos="animation-scale-y" data-aos-duration="500" className="title font-header text-center text-6xl md:text-7xl 2xl:text-8xl font-semibold bg-clip-text text-transparent bg-edu-orange z-[3] text-shadow-white">
+        <h2>{title}</h2>
       </div>
-    </>
-  )
-}
+    </div>
+    <div className="content my-12 p-8 md:p-0">{children}</div>
+  </div>
+</div>
 
 export default Education
--- containers/sections/portfolio.tsx
+++ containers/sections/portfolio.tsx
@@ -1,41 +1,24 @@
 import { type TimelineSectionsType } from 'Containers/types/sections.type'
 import styles from './section.module.scss'
-import { useState, useEffect } from 'react'
 
-const Portfolio = ({ title, desc, children }: TimelineSectionsType): JSX.Element => {
-  //  (384 * 2.5) + (isMobile ? 192 * 3.5 : 0)
-  const [isMobile, setIsMobile] = useState(false)
-
-  useEffect(() => {
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i.test(
-        navigator.userAgent
-      )
-    ) {
-      setIsMobile(true)
-    }
-  }, [])
-  return (
-    <>
-      <div className="portfolio-timeline-sections h-[calc(100vmax+360vmax)] md:h-[300vmax] lg:h-[255vmax] xl:h-[190vmax] 2xl:h-[calc(100vmax+40vmax)] portfolio-timeline-sections w-100 porto-clip-mweb md:porto-clip relative flex items-center justify-center">
-        <div className="absolute z-[2] h-full w-full md:bg-white-vertical-gradient" />
-        <div className={`absolute z-[1] h-full w-full ${styles.Section_portofolio}${!isMobile ? '' : ` ${styles.Section_portofolio_mobile}`}`} />
-        <div className="portfolio-timeline-sections-child container absolute z-[3] mx-auto h-full py-[12rem] md:py-[24rem]">
-          <div className="head h-72 md:h-[15%] text-left p-8 pb-0 md:p-0">
-            <div className="title font-header text-2xl md:text-3xl 2xl:text-4xl font-bold text-white">
-              <h2 data-aos="fade-up" data-aos-duration="700">{title}</h2>
-            </div>
-            <div data-aos="animation-scale-y" data-aos-duration="900" className="desc my-6 font-header text-6xl md:text-7xl 2xl:text-8xl font-semibold text-white">
-              {desc}
-            </div>
-          </div>
-          <div className="content grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20">
-            {children}
-          </div>
+const Portfolio = ({ title, desc, children }: TimelineSectionsType): JSX.Element =>
+  (
+  <div className="portfolio-timeline-sections h-[calc(100vmax+360vmax)] md:h-[300vmax] lg:h-[255vmax] xl:h-[190vmax] 2xl:h-[calc(100vmax+40vmax)] portfolio-timeline-sections w-100 porto-clip-mweb md:porto-clip relative flex items-center justify-center">
+    <div className="absolute z-[2] h-full w-full md:bg-white-vertical-gradient" />
+    <div className={`absolute z-[1] h-full w-full ${styles.Section_portofolio}`} />
+    <div className="portfolio-timeline-sections-child container absolute z-[3] mx-auto h-full py-[12rem] md:py-[24rem]">
+      <div className="head h-72 md:h-[15%] text-left p-8 pb-0 md:p-0">
+        <div className="title font-header text-2xl md:text-3xl 2xl:text-4xl font-bold text-white">
+          <h2 data-aos="fade-up" data-aos-duration="700">{title}</h2>
+        </div>
+        <div data-aos="animation-scale-y" data-aos-duration="900" className="desc my-6 font-header text-6xl md:text-7xl 2xl:text-8xl font-semibold text-white">
+          {desc}
         </div>
       </div>
-    </>
-  )
-}
+      <div className="content grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20">
+        {children}
+      </div>
+    </div>
+  </div>)
 
 export default Portfolio
