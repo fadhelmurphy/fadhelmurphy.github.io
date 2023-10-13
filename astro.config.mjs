877996a30a58aca7ea4a4d6e0d9b8f663838f1cd
Fadhel ngepush ⚡ <fadhelijlalfalah@gmail.com>
2023-10-13 19:09:49 +0700

    article: update component library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,29 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import image from "@astrojs/image";
-import { prefix } from "./consts/config";
+import image from '@astrojs/image'
+import { prefix } from './consts/config'
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+    // Choose from Shiki's built-in themes (or add your own)
+    // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'one-dark-pro',
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  },
   integrations: [mdx(), tailwind(), react(), image()],
   // site: `http://localhost:3000${prefix}`,
   base: prefix, // base prefix
-  assetsPrefix: prefix, // asset prefix
+  assetsPrefix: prefix // asset prefix
   // publicDir: prefix,
   // build: {
   //   assetsPrefix: prefix
   // },
   // output: 'server',
-});
+})
