7c390dd3c58171b6339dcdc8b435d517d452447d
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2023-10-14 18:59:14 +0700

    chore: update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,37 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-
-import image from '@astrojs/image'
 import { prefix } from './consts/config'
+import remarkToc from 'remark-toc'
+import rehypeMinifyHtml from 'rehype-preset-minify'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-    // Choose from Shiki's built-in themes (or add your own)
-    // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
       theme: 'one-dark-pro',
       // Enable word wrap to prevent horizontal scrolling
       wrap: true
     }
   },
-  integrations: [mdx(), tailwind(), react(), image()],
+  server: { port: 3000, host: true },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: { theme: 'dracula' },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeMinifyHtml],
+    remarkRehype: { footnoteLabel: 'Footnotes' },
+    gfm: false
+  }), tailwind(), react()],
   // site: `http://localhost:3000${prefix}`,
-  base: prefix, // base prefix
+  output: 'static',
+  base: prefix,
+  // base prefix
   assetsPrefix: prefix // asset prefix
   // publicDir: prefix,
   // build: {
   //   assetsPrefix: prefix
   // },
-  // output: 'server',
 })
