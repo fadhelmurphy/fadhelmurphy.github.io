e251ea4733a3e6d10b930aacdccb15d11eb43f92
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2024-02-08 01:29:25 +0700

    feat: add dynamic menu

--- components/mylogo/mylogo.tsx
+++ components/mylogo/mylogo.tsx
@@ -1,8 +1,18 @@
 import logo from '@/Assets/img/menu/logo.png'
+import { useState, useEffect } from 'react'
 
 function MyLogo (): JSX.Element {
+  const [isHeaderScroll, setHeaderScroll] = useState<boolean>(true)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', function (e) {
+        // print "false" if direction is down and "true" if up
+        setHeaderScroll(this?.scrollY < 1100)
+      })
+    }
+  }, [])
   return (
-    <a href="/" className="mylogo">
+    <a href="/" className={`mylogo ${!isHeaderScroll ? 'logo-hide' : ''}`}>
         <img src={logo.src} />
         <p>Fadhel</p>
     </a>
