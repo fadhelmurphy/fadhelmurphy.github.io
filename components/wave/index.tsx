cbc9b1b2c6130f09c90046a04da901b32abce246
Fadhel ngepush ⚡ <fadhelijlalfalah@gmail.com>
2024-06-17 20:01:14 +0700

    style: update styling blog list

--- components/wave/index.tsx
+++ components/wave/index.tsx
@@ -9,9 +9,9 @@ viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
 <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
 </defs>
 <g className="parallax">
-<use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-<use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-<use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.4)" />
+<use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.3)" />
+<use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.2)" />
 <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
 </g>
 </svg>
