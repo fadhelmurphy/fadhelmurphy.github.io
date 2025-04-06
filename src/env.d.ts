cc520210633f0b7b8733b8ca0d28a3cda3eb57c4
Fadhel ngepush 🚀 <fadhelijlalfalah@gmail.com>
2025-04-06 19:23:08 +0700

    chore: env for SEO purpose

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
-/// <reference types="astro/client" />
+export {};
+
+declare global {
+  interface ImportMetaEnv {
+    readonly PUBLIC_PREFIX: string;
+    readonly PUBLIC_BASE_PORT: string;
+    readonly PUBLIC_BASE_DOMAIN: string;
+    readonly PUBLIC_BASE_PROTOCOL: string;
+    readonly PUBLIC_ROOT_DOMAIN: string;
+    readonly SITE_FOR_BUILD: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
